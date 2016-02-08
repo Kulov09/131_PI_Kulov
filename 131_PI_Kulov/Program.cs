dc82ad805cb36e966c55a87a063cc2643d49f20c
Kulov09 <kulov.r@icloud.com>
2016-02-08 12:24:43 +0300

    Первое задание

--- 131_PI_Kulov/Program.cs
+++ 131_PI_Kulov/Program.cs
@@ -10,6 +10,28 @@ namespace _131_PI_Kulov
     {
         static void Main(string[] args)
         {
+            int a;
+            int b;
+            Console.WriteLine("Размер дневной выручки равен");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a <= 1000)
+            {
+                b = a / 100 * 5;
+                Console.WriteLine("Размер выручки равен");
+                Console.Write(b);
+            }
+
+            if (a > 10000)
+            {
+                b = a / 100 * 10 + 500;
+                Console.WriteLine("Размер выручки равен");
+                Console.WriteLine(b);
+            }
+            Console.ReadKey();
+
+                
+
+             
         }
     }
 }
